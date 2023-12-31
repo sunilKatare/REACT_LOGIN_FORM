c0193976dc2832c759a680c00321537fd4060d56
sunilKatare <100280298+sunilKatare@users.noreply.github.com>
2023-12-31 17:41:45 +0530

    ReactJSLogin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+//import RegistrationForm from './Components/RegistrationForm';
+import Register from './Components/Register';
+function App() {
+  return (
+    <div className="App">      
+        <Register/>      
+    </div>
+  );
+}
+
+export default App;
+  
